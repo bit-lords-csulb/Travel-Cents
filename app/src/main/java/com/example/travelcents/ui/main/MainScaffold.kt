8cf003087c2efeb6a491558b1dd0ca17252e8ec3
Danthecalfman <spikeeboy@outlook.com>
2026-03-13 14:12:09 -0700

    Update MainScaffold navigation to display ItineraryScreen
    - Replaced `CurrentPage` with `ItineraryScreen` for the `MainRoutes.Current` destination in the navigation host.

--- app/src/main/java/com/example/travelcents/ui/main/MainScaffold.kt
+++ app/src/main/java/com/example/travelcents/ui/main/MainScaffold.kt
@@ -81,7 +81,8 @@ fun MainScaffold(modifier: Modifier = Modifier) {
                 startDestination = MainRoutes.Home,
                 modifier = Modifier.fillMaxSize()
             ) {
-                composable(MainRoutes.Current) { CurrentPage(modifier = Modifier.fillMaxSize()) }
+               // composable(MainRoutes.Current) { CurrentPage(modifier = Modifier.fillMaxSize()) }
+                composable(MainRoutes.Current) { ItineraryScreen() }
                 composable(MainRoutes.NewTrip) { NewTripPage(modifier = Modifier.fillMaxSize(), viewModel = newTripViewModel) }
                 composable(MainRoutes.Home) { HomePage(modifier = Modifier.fillMaxSize()) }
                 composable(MainRoutes.Chats) { ChatsPage(modifier = Modifier.fillMaxSize()) }
